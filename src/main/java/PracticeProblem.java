704b081153cbe5ebd754415b106e2ae2473d1d00
SalmaDanane <343249595@tdsb.ca>
2026-06-01 19:13:51 +0000

    culminating

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,50 @@
+/*
+ 
+	Author - Salma Danane
+	File Name - Computer Science Culminating; text based guessing game
+	Date Created - June 1st, 2026
+	Date Last Modified - June 10th, 2026
+
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String[] args[]) { // is this technically a function (string[])
 
-	}
+		Scanner input = new Scanner(System.in); // input
+		Random r = new Random(); // used to generate a ran numeber
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+		// arraylist of words for the user to guess
+		String[] words = {"eagle", "apple", "corny", "quirk", "gnome", "cruet", "okapi"}; // list of a mix of unique and common words, this will be the list of words that the user will have to guess
+  
+		// my variables
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+		int rounds; // number of rounds user wants to play
+		int maxAttempts = 6; // maximume number of attempts user has at guessing the word
+		int picker; // this will be the number used to pick the word in the words list
+		
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		String word2Guess = // function to pick the word
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		// Game introduction text
+		System.out.println("-----WORDY-----");
+		System.out.println("Welcome to the Wordy game!");
+		System.out.println("-----GAME RULES-----");
+		System.out.println("- Guess the random 5 letter word/n- Only full 5 letter words excepted (no less, no more)/n- No symbols, charactors, or numbers accepted/n- Have fun!")
+		System.out.println("How many rounds would you like to play?");
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+
+		
+	//  start w a if statment - if they want to play run the program, if no ask them again if they want to play
+	// like in problem set unit 4
+
+
+	   
+
 
 }
